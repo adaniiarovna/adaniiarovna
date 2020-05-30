c0df720da5d17037e03edfd1b85f4877678d33c5
Bekbolot Tazhibaev <tabekg@gmail.com>
2020-05-30 16:16:00 +0600

    chat deploy

---
+++ precache-manifest.6445341a8581c2139acd3bd38d176d25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfbf8091157e66aed174d86fdd7016e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c900d830ebcc56fc900a",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "71907487376b423f1beb",
+    "url": "/static/css/main.5cecc10d.chunk.css"
+  },
+  {
+    "revision": "c900d830ebcc56fc900a",
+    "url": "/static/js/2.bb3fd710.chunk.js"
+  },
+  {
+    "revision": "5298b0565bb315b9314a8baf33940c9d",
+    "url": "/static/js/2.bb3fd710.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71907487376b423f1beb",
+    "url": "/static/js/main.15bae043.chunk.js"
+  },
+  {
+    "revision": "92cc11d29e0b43a3049e",
+    "url": "/static/js/runtime-main.7cd9a455.js"
+  }
+]);
